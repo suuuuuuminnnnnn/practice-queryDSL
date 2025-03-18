b159cae354f4a9573944c39e5c340248e916ebc7
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-18 14:16:51 +0900

    init

---
+++ src/main/java/practice/querydsl/productOrderSystem/ProductOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package practice.querydsl.productOrderSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/querydsl/productOrderSystem/ProductOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.querydsl.productOrderSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
