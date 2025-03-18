b1787e89477e0047fe60e21f62a5069991d0f1bc
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-18 18:06:54 +0900

    feat: CustomUserDetails 추가

--- src/main/java/practice/querydsl/productOrderSystem/global/security/auth/CustomUserDetails.java
+++ src/main/java/practice/querydsl/productOrderSystem/global/security/auth/CustomUserDetails.java
@@ -1,4 +1,24 @@
 package practice.querydsl.productOrderSystem.global.security.auth;
 
-public class CustomUserDetails {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
 }
