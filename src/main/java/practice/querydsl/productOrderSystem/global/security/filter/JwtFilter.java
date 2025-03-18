0018da264ac1c68e70c19d59633f09de099c2a17
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-18 19:36:13 +0900

    feat: JwtFilter 추가

---
+++ src/main/java/practice/querydsl/productOrderSystem/global/security/filter/JwtFilter.java
@@ -0,0 +1,38 @@
+package practice.querydsl.productOrderSystem.global.security.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import practice.querydsl.productOrderSystem.global.security.jwt.TokenProvider;
+
+import java.io.IOException;
+import java.security.Security;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+        if (authHeader != null) {
+            authHeader = authHeader.replace(BEARER_PREFIX, "").trim();
+
+            UsernamePasswordAuthenticationToken authenticationToken = tokenProvider.getAuthentication(authHeader);
+            SecurityContextHolder.clearContext();
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
