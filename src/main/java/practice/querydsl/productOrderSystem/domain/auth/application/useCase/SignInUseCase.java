cf93bac0b4e14ef7b431bdfc23dbe7f1703d8607
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-20 18:29:51 +0900

    feat: SignInUseCase 추가

---
+++ src/main/java/practice/querydsl/productOrderSystem/domain/auth/application/useCase/SignInUseCase.java
@@ -0,0 +1,7 @@
+package practice.querydsl.productOrderSystem.domain.auth.application.useCase;
+
+import practice.querydsl.productOrderSystem.domain.auth.presentation.data.SignInResponse;
+
+public interface SignInUseCase {
+    SignInResponse execute(String email, String password);
+}
