7c173ebc91877b2080f8a40e04c1265d4bd6bd57
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-24 15:20:15 +0900

    feat: UpdateProductRequest 추가

---
+++ src/main/java/practice/querydsl/productOrderSystem/domain/product/presentation/data/request/UpdateProductRequest.java
@@ -0,0 +1,11 @@
+package practice.querydsl.productOrderSystem.domain.product.presentation.data.request;
+
+import practice.querydsl.productOrderSystem.domain.product.domain.type.ProductCategory;
+
+public record UpdateProductRequest(
+        Long id,
+        ProductCategory category,
+        String name,
+        String description,
+        Long price) {
+}
