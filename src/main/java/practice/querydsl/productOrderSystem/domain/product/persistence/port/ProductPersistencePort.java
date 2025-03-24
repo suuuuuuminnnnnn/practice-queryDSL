0ed85199f25f3157b5ed3520524120daa7fac030
suuuuuuminnnnnn <s24035@gsm.hs.kr>
2025-03-24 11:57:28 +0900

    feat: ProductPersistencePort 추가

--- src/main/java/practice/querydsl/productOrderSystem/domain/product/persistence/port/ProductPersistencePort.java
+++ src/main/java/practice/querydsl/productOrderSystem/domain/product/persistence/port/ProductPersistencePort.java
@@ -1,7 +1,19 @@
 package practice.querydsl.productOrderSystem.domain.product.persistence.port;
 
+import practice.querydsl.productOrderSystem.domain.business.domain.Business;
 import practice.querydsl.productOrderSystem.domain.product.domain.Product;
+import practice.querydsl.productOrderSystem.domain.product.domain.type.ProductCategory;
+
+import java.util.List;
 
 public interface ProductPersistencePort {
     Product findProductByProductId(Long productId);
+
+    List<Product> findAllProducts();
+
+    void saveProduct(Product product);
+
+    void deleteProduct(Long productId);
+
+    List<Product> findProductsByCategoryAndNameAndPriceRangeAndBusiness(ProductCategory category, String name, Long minPrice, Long maxPrice, Business business);
 }
